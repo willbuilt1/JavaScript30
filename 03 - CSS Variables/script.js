7940f3d8d00d3239cb47717617e30f1361ecf37e
Will <wfoster12@gmail.com>
2018-07-19 16:50:08 +0100

    completed exercise 3 - see readme for details

---
+++ 03 - CSS Variables/script.js
@@ -0,0 +1,25 @@
+//Look for inputs that are children of class controls
+const inputs = document.querySelectorAll('.controls input');
+const img = document.querySelector('img');
+
+//Set image width
+function setImageWidth(){
+    const padding = document.querySelector('#spacing').value;
+    const imgWidth= (window.innerWidth)- (2*padding)-50;
+    img.style.width = `${imgWidth}px`
+}
+
+//deal with change
+function handleUpdate(){
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+    setImageWidth();
+}
+
+//listen for change on each of the inputs
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+window.addEventListener('resize', setImageWidth);
+
+//set image width on load
+setImageWidth();
--- readme.md
+++ readme.md
@@ -1,2 +1,12 @@
-# JavaScript30
+# **JavaScript30**
+
+Repo for the Javascript30 course from @wesbos.
+ 
+In each exercise I try to improve on the final solution. Below is listed the changes to the final solutions on the course.
+
+**Exercise 3:**
+ - Made the page responsive using flexbox and a media query
+ - Used JavaScript to set the initial size of the image depending on the width of the window. 
+ - In the same function the image would then be shrunk depending on the padding amount. CSS styling could not be used in this instance as an increase in padding would push the image outside of the window width (on smaller devices).
+
 
